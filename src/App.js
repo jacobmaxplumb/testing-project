7e2db2acab56448ad8364b641e2678514cc21aab
Jacob Plumb <jacobmaxplumb@gmail.com>
2024-06-13 18:28:23 -0400

    made todo list component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+  const addTodoItem = () => {
+    setTodos([...todos, text]);
+    setText('');
+  }
+
   return (
     <div className="App">
-      <h1>My Testing App</h1>
+      <input value={text} onChange={(e) => setText(e.target.value)}/>
+      <button onClick={addTodoItem}>Add</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
